8f66018a13bdfe977fe572a3a4d1b6d43acfbb26
Rey-Diaz <Reinoldiaz@gmail.com>
2023-08-21 12:03:59 -0400

    init push

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            © 2023 AI Fitness - All Rights Reserved
+        </footer>
+    );
+}
+
+export default Footer;
